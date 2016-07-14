f5e9bf955240b329bfea994e01fbe9c05b3959f8
Frederic Aerts <frederic@faerts.local>
2016-07-14 13:49:05 +0200

    optimize add players to game modal

--- js/app/home/home.controller.js
+++ js/app/home/home.controller.js
@@ -10,18 +10,9 @@
     function HomeCtrl($scope, $state, playersService, gameService, TEMPLATES_ROOT, $ionicModal) {
         /* jshint validthis: true */
         var vm = this;
+        var addPlayersToGameModalScope = $scope.$new(true);
 
         vm.addPlayersToGameModal = {};
-        vm.cancelAddPlayersToGame = cancelAddPlayersToGame;
-        vm.playersInDatabase = playersService.all();
-        vm.participants = [];
-        vm.showReorder = false;
-        vm.addPlayerToParticipants = addPlayerToParticipants;
-        vm.removePlayerFromParticipants = removePlayerFromParticipants;
-        vm.reorderParticipant = reorderParticipant;
-        vm.guestColors = ['blonde', 'orange', 'pink', 'white', 'brown', 'blue'];
-        vm.addGuestParticipant = addGuestParticipant;
-        vm.startGame = startGame;
 
         activate();
 
@@ -35,72 +26,82 @@
             ======================================================================================== */
         // Cleanup the modal when we're done with it!
         $scope.$on('$destroy', function() {
-            $scope.modal.remove();
+            vm.addPlayersToGameModal.remove();
         });
 
         /*  FUNCTIONS
             ======================================================================================== */
         function initAddPlayersToGameModal() {
+            /*  This modal gets a shallow copy of players from playersService as input,
+                and outputs a shallow copy of participants to gameService
+                ============================================================================*/
             $ionicModal.fromTemplateUrl(TEMPLATES_ROOT + '/home/modal-start-players.html', {
-                scope: $scope,
+                scope: addPlayersToGameModalScope,
                 animation: 'slide-in-up'
             })
             .then(function(modal) {
                 vm.addPlayersToGameModal = modal;
             });
-        }
 
-        function cancelAddPlayersToGame() {
-            vm.showReorder = false;
+            // modal template should reference 'viewModel' as its scope
+            addPlayersToGameModalScope.viewModel = {
+                showReorder: false,
+                guestColors: ['blonde', 'orange', 'pink', 'white', 'brown', 'blue'],
+                playersInDatabase: playersService.all().slice(), // modal input
+                participants: [], // modal output
+                addPlayerToParticipants: addPlayerToParticipants,
+                addGuestParticipant: addGuestParticipant,
+                removePlayerFromParticipants: removePlayerFromParticipants,
+                reorderParticipant: reorderParticipant,
+                cancelAddPlayersToGame: cancelAddPlayersToGame,
+                startGame: startGame
+            };
 
-            // empty participants array
-            for (var i = vm.participants.length - 1; i >= 0; --i) { // backwards loop
-                if (vm.participants[i].guestColor) {
-                    vm.guestColors.push(vm.participants.pop().guestColor);
-                }
-                else {
-                    vm.playersInDatabase.push(vm.participants.pop());
-                }
-            }
-
-            vm.addPlayersToGameModal.hide();
+            addPlayersToGameModalScope.$on('modal.hidden', function() {
+                resetAddPlayersToGameModalData();
+            });
         }
 
         function addPlayerToParticipants(newParticipant) {
-            var playerIndex = _.findIndex(vm.playersInDatabase, function(player) {
+            var playerIndex = _.findIndex(addPlayersToGameModalScope.viewModel.playersInDatabase, function(player) {
                 return player.name === newParticipant.name;
             });
-            vm.participants.push(vm.playersInDatabase.splice(playerIndex, 1)[0]);
+            var playerFromDatabase = addPlayersToGameModalScope.viewModel.playersInDatabase.splice(playerIndex, 1)[0];
+            addPlayersToGameModalScope.viewModel.participants.push(playerFromDatabase);
         }
 
         function removePlayerFromParticipants(index) {
-            var removedPlayer = vm.participants.splice(index, 1)[0];
+            var removedPlayer = addPlayersToGameModalScope.viewModel.participants.splice(index, 1)[0];
 
             if (removedPlayer.guestColor) {
-                vm.guestColors.push(removedPlayer.guestColor);
+                addPlayersToGameModalScope.viewModel.guestColors.push(removedPlayer.guestColor);
             }
             else {
-                vm.playersInDatabase.push(removedPlayer);
+                addPlayersToGameModalScope.viewModel.playersInDatabase.push(removedPlayer);
             }
         }
 
         function reorderParticipant(player, fromIndex, toIndex) {
-            vm.participants.splice(fromIndex, 1);
-            vm.participants.splice(toIndex, 0, player);
+            addPlayersToGameModalScope.viewModel.participants.splice(fromIndex, 1);
+            addPlayersToGameModalScope.viewModel.participants.splice(toIndex, 0, player);
         }
 
         function addGuestParticipant() {
             var guestColor = pickRandomGuestColor();
 
-            vm.participants.push({
+            addPlayersToGameModalScope.viewModel.participants.push({
                 name: 'Mr. ' + capitalizeFirstLetter(guestColor),
                 face: '',
                 guestColor: guestColor
             });
         }
 
+        function cancelAddPlayersToGame() {
+            vm.addPlayersToGameModal.hide();
+        }
+
         function startGame() {
-            gameService.setParticipants(vm.participants);
+            gameService.setParticipants(addPlayersToGameModalScope.viewModel.participants.slice());
 
             $state.go('game');
 
@@ -110,13 +111,28 @@
         /*  Helper functions
             ======================================================================================== */
         function pickRandomGuestColor() {
-            var colorIndex = Math.floor(Math.random() * vm.guestColors.length);
+            var colorIndex = Math.floor(Math.random() * addPlayersToGameModalScope.viewModel.guestColors.length);
 
-            return vm.guestColors.splice(colorIndex, 1)[0];
+            return addPlayersToGameModalScope.viewModel.guestColors.splice(colorIndex, 1)[0];
         }
 
         function capitalizeFirstLetter(string) {
             return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
+        function resetAddPlayersToGameModalData() { // reset playersInDatabase & participants
+            addPlayersToGameModalScope.viewModel.showReorder = false;
+
+            // empty participants array
+            for (var i = addPlayersToGameModalScope.viewModel.participants.length - 1; i >= 0; --i) { // backwards loop
+                var playerPoppedFromParticipants =  addPlayersToGameModalScope.viewModel.participants.pop();
+                if (playerPoppedFromParticipants.guestColor) {
+                    addPlayersToGameModalScope.viewModel.guestColors.push(playerPoppedFromParticipants.guestColor);
+                }
+                else {
+                    addPlayersToGameModalScope.viewModel.playersInDatabase.push(playerPoppedFromParticipants);
+                }
+            }
         }
     }
 })();
