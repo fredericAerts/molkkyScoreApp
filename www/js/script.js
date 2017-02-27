06258ad07119ffd750094d5547102bd6fb772e01
Frederic Aerts <info@fredericaerts.com>
2017-02-27 14:25:20 +0100

    tweak resource icons

--- www/js/script.js
+++ www/js/script.js
@@ -7094,7 +7094,6 @@ angular.module('molkkyscore', ['ionic', 'ngCordova', 'pascalprecht.translate']);
         function initPlayers() {
             var selectAllPlayersQuery = 'SELECT * FROM PLAYERS';
             return $cordovaSQLite.execute(database, selectAllPlayersQuery).then(function(res) {
-                console.log(JSON.stringify(res));
                 for (var i = 0; i < res.rows.length; i++) {
                     var row = res.rows.item(i);
                     players.push({
